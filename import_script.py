ed2f7646293f30bfd997c72de2e7b761db941588
Shawn-Philippe Levasseur <levasseur.sp@gmail.com>
2019-03-15 09:45:22 -0400

    Created an s3 class

--- import_script.py
+++ import_script.py
@@ -1,3 +1,17 @@
-from s3.utils import read_data_s3
+import os
+from s3.utils import s3
+from dotenv import load_dotenv
 
-print(read_data_s3('Dkt_canada/data/sport_popularity/city_sport_2019-03-13_000.gz'))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path=ENV_PATH)
+
+client = s3(aws_key=os.getenv("AWS_ACCESS_KEY_ID"),
+            secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            bucket=os.getenv("BUCKET"))
+
+dataframe = client.read_data_s3('Dkt_canada/data/sport_popularity/city_sport_2019-03-13_000.gz')
+
+print(
+    dataframe
+    )
--- s3/utils.py
+++ s3/utils.py
@@ -8,63 +8,65 @@ import boto3
 import pandas as pd
 
 # Setting up the variables for s3
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-BUCKET = os.getenv("BUCKET")
 COMPRESSION = "gzip"
 REGION_NAME = "EU-west-1"
 
 
-def read_data_s3(file_path):
-    """ Reads a compressed file in an s3 bucket
+class s3:
+    def __init__(self, aws_key, secret_key, bucket):
+        self.aws_key = aws_key
+        self.secret_key = secret_key
+        self.bucket = bucket
 
-    Parameters:
-    file_path (string): The path of the compressed csv in the bucket
+    def read_data_s3(self, file_path):
+        """ Reads a compressed file in an s3 bucket
 
-    Returns:
-    data_frame (DataFrame() from pandas): The data from the compressed file in the s3 bucket
+        Parameters:
+        file_path (string): The path of the compressed csv in the bucket
 
-    """
-    session = boto3.session.Session(region_name=REGION_NAME)
-    s3client = session.client(
-        "s3",
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    )
+        Returns:
+        data_frame (DataFrame() from pandas): The data from the compressed file in the s3 bucket
 
-    try:
-        response = s3client.get_object(Bucket=BUCKET, Key=file_path)
-    except s3client.exceptions.NoSuchKey as err:
-        return print(err)
+        """
+        session = boto3.session.Session(region_name=REGION_NAME)
+        s3client = session.client(
+            "s3",
+            aws_access_key_id=self.aws_key,
+            aws_secret_access_key=self.secret_key,
+        )
 
-    data_frame = pd.read_csv(
-        io.BytesIO(response["Body"].read()),
-        sep=";",
-        header=0,
-        compression=COMPRESSION
-    )
+        try:
+            response = s3client.get_object(Bucket=self.bucket, Key=file_path)
+        except s3client.exceptions.NoSuchKey as err:
+            return print(err)
 
-    return data_frame
+        data_frame = pd.read_csv(
+            io.BytesIO(response["Body"].read()),
+            sep=";",
+            header=0,
+            compression=COMPRESSION
+        )
 
+        return data_frame
 
-def save_to_resources(filepath, data_frame):
-    """ Saves the data_frame to a specified filepath
+    def save_to_resources(filepath, data_frame):
+        """ Saves the data_frame to a specified filepath
 
-    Parameters:
-    filepath (string): The path where to save the .csv to
-    data_frame (DataFrame from pandas): The data frame with all the product's information
+        Parameters:
+        filepath (string): The path where to save the .csv to
+        data_frame (DataFrame from pandas): The data frame with all the product's information
 
-    """
-    # If the resources directory doesn't exist, create it
-    if not os.path.exists(os.path.dirname(filepath)):
-        os.makedirs(os.path.dirname(filepath))
+        """
+        # If the resources directory doesn't exist, create it
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath))
 
-    # If the .csv file doesn't exist, create it
-    if not os.path.exists(filepath):
-        file = open(filepath, mode="w")
-        file.close()
+        # If the .csv file doesn't exist, create it
+        if not os.path.exists(filepath):
+            file = open(filepath, mode="w")
+            file.close()
 
-    mdata = data_frame.fillna(0.0)
+        mdata = data_frame.fillna(0.0)
 
-    mdata.index = np.arange(1, len(mdata) + 1)
-    mdata.to_csv(filepath, index_label="id", sep=",", encoding="utf-8")
+        mdata.index = np.arange(1, len(mdata) + 1)
+        mdata.to_csv(filepath, index_label="id", sep=",", encoding="utf-8")
