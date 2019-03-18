f35f45fa0b3801791d14732ce5d821de5cea6f9e
Shawn-Philippe Levasseur <levasseur.sp@gmail.com>
2019-03-18 16:08:33 -0400

    Possible usage

--- import_script.py
+++ import_script.py
@@ -1,17 +1,18 @@
 import os
 from s3.utils import s3
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=ENV_PATH)
 
+YESTERDAY= (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
+
 client = s3(aws_key=os.getenv("AWS_ACCESS_KEY_ID"),
             secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
             bucket=os.getenv("BUCKET"))
 
-dataframe = client.read_data_s3('Dkt_canada/data/sport_popularity/city_sport_2019-03-13_000.gz')
+dataframe = client.read_data_s3('Dkt_canada/data/sport_popularity/city_sport_%s_000.gz' % (YESTERDAY))
 
-print(dataframe)
-
-# client.save_to_csv('./sports.csv', dataframe)
+client.save_to_csv('./sports.csv', dataframe)
