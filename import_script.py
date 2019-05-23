31a97a19ba2392d3cc1a62165b7de9926f40260f
Levasseur-Sp <shawnphilippe.levasseur@decathlon.com>
2019-05-23 12:41:15 -0400

    Modify the testing script

--- import_script.py
+++ import_script.py
@@ -7,12 +7,16 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=ENV_PATH)
 
-YESTERDAY= (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
+YESTERDAY = (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
 
-client = s3(aws_key=os.getenv("AWS_ACCESS_KEY_ID"),
-            secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            bucket=os.getenv("BUCKET"))
+client = s3(
+    aws_key=os.getenv("AWS_ACCESS_KEY_ID"),
+    secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    bucket=os.getenv("BUCKET")
+)
 
-df = client.convert_s3_csv_to_df(key='Dkt_canada/data/sport_popularity/city_sport_%s_000.gz' % (YESTERDAY))
+df = client.convert_s3_csv_to_df(
+    key=f'Dkt_canada/data/sport_popularity/city_sport_{YESTERDAY}_000.gz'
+)
 
 client.save_to_csv(df, filepath='./sports.csv')
