926c4108ac47c03fdf6ae90ac401b5de9ee4213b
Levasseur-Sp <shawnphilippe.levasseur@decathlon.com>
2019-05-23 12:37:18 -0400

    Make some functions and vars private
    Nothing is truly private in python but the functions and variables
    starting with a '_' are considered private.
    
    Change double-quotes to single-quotes to follow the team convention that
    was chosen.
    
    Compression and region_names are now class variables to permit the
    library user to change them if needed.

--- s3/utils.py
+++ s3/utils.py
@@ -1,7 +1,3 @@
-"""
-This module contains helper functions to import data from an s3 bucket to a .csv
-(For refactoring purposes)
-"""
 import io
 import os
 
@@ -10,73 +6,79 @@ import tablib
 import pandas as pd
 import numpy as np
 
-if os.getenv("DJANGO_SETTINGS_MODULE"):
+# If it's a django project
+if os.getenv('DJANGO_SETTINGS_MODULE'):
     from import_export import resources
-
-# Setting up the variables for s3
-COMPRESSION = "gzip"
-REGION_NAME = "EU-west-1"
 
 
 class s3:
-    def __init__(self, aws_key=None, secret_key=None, bucket=None):
-        self.aws_key = aws_key
-        self.secret_key = secret_key
-        self.bucket = bucket
+    def __init__(
+            self,
+            aws_key,
+            secret_key,
+            bucket,
+            compression='gzip',
+            region='EU-west-1'
+    ):
+        self._aws_key = aws_key
+        self._secret_key = secret_key
+        self._bucket = bucket
+        self._compression = compression
+        self._region = region
 
     def convert_s3_csv_to_df(self, key):
         # Connect to the s3 bucket and extract the compressed csv at the key
-        session = boto3.session.Session(region_name=REGION_NAME)
+        session = boto3.session.Session(region_name=self._region)
         s3client = session.client(
-            "s3",
-            aws_access_key_id=self.aws_key,
-            aws_secret_access_key=self.secret_key,
+            's3',
+            aws_access_key_id=self._aws_key,
+            aws_secret_access_key=self._secret_key,
         )
-        response = s3client.get_object(Bucket=self.bucket, Key=key)
+        response = s3client.get_object(Bucket=self._bucket, Key=key)
 
         df = pd.read_csv(
-            io.BytesIO(response["Body"].read()),
-            sep=";",
+            io.BytesIO(response['Body'].read()),
+            sep=';',
             header=0,
-            compression=COMPRESSION
+            compression=self._compression
         )
 
         return df
 
     def save_to_csv(self, df, filepath='./'):
-        self.create_filepath_if_nonexistent(filepath)
+        self._create_filepath_if_nonexistent(filepath)
 
         df.fillna(0.0, inplace=True)
         df.index = np.arange(1, len(df)+1)
-        df.to_csv(filepath, index_label="id", sep=",", encoding="utf-8")
+        df.to_csv(filepath, index_label='id', sep=',', encoding='utf-8')
 
-    def create_filepath_if_nonexistent(self, filepath):
+    def _create_filepath_if_nonexistent(self, filepath):
         # If the resources directory doesn't exist, create it
         if not os.path.exists(os.path.dirname(filepath)):
             os.makedirs(os.path.dirname(filepath))
 
         # If the .csv file doesn't exist, create it
         if not os.path.exists(filepath):
-            file = open(filepath, mode="w")
+            file = open(filepath, mode='w')
             file.close()
 
     def convert_df_to_django_model(self, df, model, rewrite=False):
-        if not os.getenv("DJANGO_SETTINGS_MODULE"):
+        if not os.getenv('DJANGO_SETTINGS_MODULE'):
             raise Exception(
-                "DJANGO_SETTINGS_MODULE is not set. This function is for Django projects."
+                'DJANGO_SETTINGS_MODULE is not set. This function is for Django projects.'
             )
 
         if rewrite:
-            self.clear_model_table(model)
+            self._clear_model_table(model)
 
         try:
             # Since Django's ORM uses incremental IDs by default
             # we need to go and take the next 'available' one
             # if the query returns none, then we start at 0
             query = model.objects.values('id').order_by('-id').first()
-            last_id = query["id"] + 1 if query is not None else 0
+            last_id = query['id'] + 1 if query is not None else 0
 
-            dataset = self.convert_df_to_dataset(df, last_id)
+            dataset = self._convert_df_to_dataset(df, last_id)
 
             # Save the data to the database
             p_resource = resources.modelresource_factory(model=model)()
@@ -84,11 +86,11 @@ class s3:
         except Exception as err:
             return print(err)
 
-    def convert_df_to_dataset(self, df, last_id):
+    def _convert_df_to_dataset(self, df, last_id):
         # Set the new ids to start with the next available one
         # Change the dataframe into a dictionnary
         # because you can't change it into a Dataset directly
-        df["id"] = range(last_id, last_id + len(df))
+        df['id'] = range(last_id, last_id + len(df))
         df.filna(0.0, inplace=True)
         headers = list(df)
         df = df.to_dict('records')
@@ -100,7 +102,7 @@ class s3:
 
         return dataset
 
-    def clear_model_table(self, model):
+    def _clear_model_table(self, model):
         try:
             model.objects.all().delete()
         except Exception as err:
