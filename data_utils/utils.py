85c753cf22a37b2a40ce86719acc928e1e885671
Levasseur-Sp <shawnphilippe.levasseur@decathlon.com>
2019-05-31 09:07:16 -0400

    Remove public functions from utils
    Utils is now a file for the library's helper functions. Will move public
    functions to other files.
    
    Remove .env.template as it doesn't serve any real purpose.

--- data_utils/utils.py
+++ data_utils/utils.py
@@ -1,74 +1,19 @@
-import io
 import os
 
-import boto3
 import tablib
-import pandas as pd
 import numpy as np
 
 
-def import_s3_csv_to_df(
-        _aws_key,
-        _secret_key,
-        _bucket,
-        _region,
-        key,
-        sep=';',
-        header=0,
-        compression='gzip'
-):
-    # Connect to the s3 bucket and extract the compressed csv at the key
-    session = boto3.session.Session(region_name=_region)
-    s3client = session.client(
-        's3',
-        aws_access_key_id=_aws_key,
-        aws_secret_access_key=_secret_key,
-    )
-    response = s3client.get_object(Bucket=_bucket, Key=key)
-
-    df = pd.read_csv(
-        io.BytesIO(response['Body'].read()),
-        sep=sep,
-        header=header,
-        compression=compression
-    )
-
-    return df
+def _get_x(phi, theta):
+    return 6367 * np.cos(phi) * np.cos(theta)
 
 
-def convert_df_to_csv(df, filepath='./', index_label='id', sep=',', encoding='utf-8'):
-    _create_filepath_if_nonexistent(filepath)
-
-    df.fillna(0.0, inplace=True)
-    df.index = np.arange(1, len(df)+1)
-    df.to_csv(
-        filepath,
-        index_label=index_label,
-        sep=sep,
-        encoding=encoding
-    )
+def _get_y(phi, theta):
+    return 6367 * np.cos(phi) * np.sin(theta)
 
 
-def convert_df_to_django_model(df, model, rewrite=False):
-    _setup_django()
-
-    if rewrite:
-        _clear_model_table(model)
-
-    try:
-        # Since Django's ORM uses incremental IDs by default
-        # we need to go and take the next 'available' one
-        # if the query returns none, then we start at 0
-        query = model.objects.values('id').order_by('-id').first()
-        last_id = query['id'] + 1 if query is not None else 0
-
-        dataset = _convert_df_to_dataset(df, last_id)
-
-        # Save the data to the database
-        p_resource = resources.modelresource_factory(model=model)()
-        p_resource.import_data(dataset)
-    except Exception as err:
-        return print(err)
+def _get_z(phi):
+    return 6367 * np.sin(phi)
 
 
 def _convert_df_to_dataset(df, last_id):
