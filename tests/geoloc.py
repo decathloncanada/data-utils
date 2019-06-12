d25fe7736b12301e3ac3ea8592d569958b79c3dc
Levasseur-Sp <shawnphilippe.levasseur@decathlon.com>
2019-06-12 13:33:37 -0400

    Organize the tests for geoloc
    in a class -> function way.

--- tests/geoloc.py
+++ tests/geoloc.py
@@ -1,8 +1,6 @@
-import os
 import unittest
 
 from dotenv import load_dotenv
-import boto3
 import pandas as pd
 from scipy import spatial
 
@@ -11,21 +9,28 @@ import data_utils.geoloc as du  # du for data_utils
 load_dotenv()
 
 
-class TestDfMethods(unittest.TestCase):
+class TestKDTree(unittest.TestCase):
 
     def setUp(self):
         data = {'latitude': [45], 'longitude': [-73]}
-        self.df = pd.DataFrame(data=data)
-        self.kd_tree = du.map_df_to_kd_tree(self.df)
-        self.exact_location = du.calculate_haversine_distance(self.df, 45, -73)
+        df = pd.DataFrame(data=data)
+        self.kd_tree = du.map_df_to_kd_tree(df)
 
-    def test_kd_tree_type(self):
+    def test_type(self):
         self.assertIsInstance(self.kd_tree, spatial.KDTree)
 
-    def test_calculate_haversine_distance_type(self):
+
+class TestCalculateHaversineDistance(unittest.TestCase):
+
+    def setUp(self):
+        data = {'latitude': [45], 'longitude': [-73]}
+        df = pd.DataFrame(data=data)
+        self.exact_location = du.calculate_haversine_distance(df, 45, -73)
+
+    def test_type(self):
         self.assertEqual(type(self.exact_location), float)
 
-    def test_haversine_same_origin(self):
+    def test_same_point_of_origin(self):
         self.assertEqual(self.exact_location, 0.0)
 
 
