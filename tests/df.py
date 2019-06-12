f410937262f4cf35084e06de119cbf273403e1b4
Levasseur-Sp <shawnphilippe.levasseur@decathlon.com>
2019-06-12 13:43:23 -0400

    Create different classes for different functions
    Add a setUp function to create a csv inside the s3 bucket and to setup
    the s3client variable that is used in every call of s3.

--- tests/df.py
+++ tests/df.py
@@ -9,33 +9,34 @@ import data_utils.df as du  # du for data_utils
 
 load_dotenv()
 
+S3CLIENT = None
 
-class TestDfMethods(unittest.TestCase):
+
+def setUp():
+    global S3CLIENT
+    # Connect to the s3 bucket and extract the compressed csv at the key
+    session = boto3.session.Session(region_name='eu-west-1')
+    S3CLIENT = session.client(
+        's3',
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    )
+
+    data = {'col1': [1, 2, 3, 4, 5], 'col2': [1, 2, 3, 4, 5]}
+    df = pd.DataFrame(data=data)
+
+    du.convert_df_to_s3_compressed_csv(df,
+                                       s3client=S3CLIENT,
+                                       bucket=os.getenv("BUCKET"),
+                                       key=f'Dkt_canada/shawn_test/test_000.gz')
+
+
+class TestImportS3CsvToDf(unittest.TestCase):
 
     def setUp(self):
-        # Connect to the s3 bucket and extract the compressed csv at the key
-        session = boto3.session.Session(region_name='eu-west-1')
-        self.s3client = session.client(
-            's3',
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        )
-
-        data = {'col1': [1, 2, 3, 4, 5], 'col2': [1, 2, 3, 4, 5]}
-        df = pd.DataFrame(data=data)
-
-        du.convert_df_to_s3_compressed_csv(df,
-                                           s3client=self.s3client,
-                                           bucket=os.getenv("BUCKET"),
-                                           key=f'Dkt_canada/shawn_test/test_000.gz')
-
-        self.df = du.import_s3_csv_to_df(s3client=self.s3client,
+        self.df = du.import_s3_csv_to_df(s3client=S3CLIENT,
                                          bucket=os.getenv("BUCKET"),
                                          key=f'Dkt_canada/shawn_test/test_000.gz')
-
-        self.list_keys = du.list_s3_keys_in_bucket(self.s3client,
-                                                   bucket=os.getenv("BUCKET"),
-                                                   prefix=f'Dkt_canada/shawn_test/')
 
     def test_check_df_type(self):
         self.assertEqual(type(self.df), pd.DataFrame)
@@ -45,6 +46,14 @@ class TestDfMethods(unittest.TestCase):
 
     def test_count_df_columns(self):
         self.assertEqual(self.df.shape[1], 2)
+
+
+class TestListS3KeysInBucket(unittest.TestCase):
+
+    def setUp(self):
+        self.list_keys = du.list_s3_keys_in_bucket(S3CLIENT,
+                                                   bucket=os.getenv("BUCKET"),
+                                                   prefix=f'Dkt_canada/shawn_test/')
 
     def test_check_list_type(self):
         self.assertEqual(type(self.list_keys), list)
@@ -57,4 +66,5 @@ class TestDfMethods(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    setUp()
     unittest.main(warnings='ignore')
